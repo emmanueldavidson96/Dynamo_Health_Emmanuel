fba538044448dfb847ddb00a60cd9e1a8ea05ef9
Emmanuel <emmanueldavidson524@gmail.com>
2024-04-01 03:13:06 +0100

    First commit

--- Dynamo_Health_Server/index.js
+++ Dynamo_Health_Server/index.js
@@ -1,30 +1,34 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const user__router = require("./Routes/UserRoutes");
-const blog__router = require("./Routes/BlogRoutes");
-const comment__router = require("./Routes/CommentRoutes");
-const cors = require("cors")
-const cookieParser = require("cookie-parser");
-const fs = require("fs")
+// const user__router = require("./Routes/UserRoutes");
+// const blog__router = require("./Routes/BlogRoutes");
+// const comment__router = require("./Routes/CommentRoutes");
+// const cors = require("cors")
+// const cookieParser = require("cookie-parser");
+// const fs = require("fs")
 
 //Application Middlewares
 const application = express(); 
 dotenv.config();
 application.use(express.json());
-application.use(cookieParser());
-application.use("/uploads", express.static(__dirname+"/uploads"));
+// application.use(cookieParser());
+// application.use("/uploads", express.static(__dirname+"/uploads"));
 
-application.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
-}))
+// application.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials: true
+// }))
 
 const PORT = process.env.PORT || 8000
 
 //Application Endpoints
 application.get("/", async (req, res) => {
     res.send("Express API for Dynamo Health")
+})
+
+application.get("/api", async (req, res) => {
+    res.send("Express API for Dynamo Health Together")
 })
 // application.use("/api/user", user__router);
 // application.use("/api/blog", blog__router);
