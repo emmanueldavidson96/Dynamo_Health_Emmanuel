2c7bbaeb77f90d038fd271a5416dd966f7ff78ba
Emmanuel <emmanueldavidson524@gmail.com>
2024-04-19 17:18:58 +0100

    little

--- Dynamo_Health_Server/index.js
+++ Dynamo_Health_Server/index.js
@@ -43,6 +43,7 @@ application.get("/api", async (req, res) => {
 application.use("/api/user", user__router);
 application.use("/api/blog", blog__router);
 application.use("/api/comment", comment__router);
+// application
 
 //MIDDLEWARE
 application.post("/api/upload",upload.single("file"),(req,res)=>{
